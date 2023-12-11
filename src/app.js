2045fe65674c7f7fe35dc1fe987bec19fb8bb69d
hidayatTM <muhtar.mis@gmail.com>
2023-12-11 14:19:48 +0700

    Menambahkan fitur payment gatway midtrans

--- src/app.js
+++ src/app.js
@@ -71,6 +71,99 @@ document.addEventListener("alpine:init", () => {
   });
 });
 
+// form validation
+
+const checkoutButton = document.querySelector(".checkout-button");
+// checkoutButton.disabled = true;
+
+// ambil form
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function (e) {
+  // telusuri masing-masing element di formnya
+  e.preventDefault();
+
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.lenght !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// kirim data ketika tombol checkout diklik
+
+// format pesan whatsapp
+
+const formatMessage = (obj) => {
+  return `Data Customer 
+    Nama : ${obj.name}
+    Email : ${obj.email}
+    No Hp : ${obj.phone}
+
+    Data Pesanan : 
+    ${JSON.parse(obj.items).map(
+      (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`
+    )}
+  TOTAL : ${rupiah(obj.total)}
+  Terima Kasih.
+  `;
+};
+
+checkoutButton.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  // konversi menjadi obejct
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  // sample payment link
+  // console.log(message);
+  // window.open("http://wa.me/6285714862217?text=" + encodeURIComponent(message));
+  // end sample with paymant link and send data via wa
+
+  // minta transaction token menggunakan ajax atau fetch
+
+  try {
+    const response = await fetch("php/placeOrder.php", {
+      method: "POST",
+      body: data,
+    });
+    const token = await response.text();
+    // console.log(token);
+    // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token
+    // window.snap.pay(token);
+    window.snap.pay(token, {
+      onSuccess: function (result) {
+        /* You may add your own implementation here */
+        alert("payment success!");
+        console.log(result);
+      },
+      onPending: function (result) {
+        /* You may add your own implementation here */
+        alert("wating your payment!");
+        console.log(result);
+      },
+      onError: function (result) {
+        /* You may add your own implementation here */
+        alert("payment failed!");
+        console.log(result);
+      },
+      onClose: function () {
+        /* You may add your own implementation here */
+        alert("you closed the popup without finishing the payment");
+      },
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
 // konversi ke Rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat("id-ID", {
